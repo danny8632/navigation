945057f14297a75150342b17eee381ccd090849e
bensandland <benjaminsandland@gmail.com>
2020-12-04 10:01:06 +0100

    fucking login screen

--- components/screens/loginScreen.js
+++ components/screens/loginScreen.js
@@ -2,8 +2,8 @@ import React, { useState } from 'react';
 import { View, StyleSheet, Text, TextInput, TouchableOpacity } from 'react-native';
 
 const LoginScreen = () => {
-    const [emailTxt, setEmailTxt] = useState('');
-    const [passwordTxt, setPasswordTxt] = useState('');
+    const [emailTxt, setEmailTxt] = React.useState('');
+    const [passwordTxt, setPasswordTxt] = React.useState('');
 
     return (
         <View style={styles.container}>
@@ -13,7 +13,8 @@ const LoginScreen = () => {
                     style={styles.inputText}
                     placeholder="Email..." 
                     placeholderTextColor="#003f5c"
-                    onChangeText={text => React.setState({email:text})}/>
+                    onChangeText={text => setEmailTxt(text)}
+                    value={emailTxt}/>
             </View>
             <View style={styles.inputView} >
                 <TextInput  
@@ -21,16 +22,17 @@ const LoginScreen = () => {
                     style={styles.inputText}
                     placeholder="Password..." 
                     placeholderTextColor="#003f5c"
-                    onChangeText={text => React.setState({password:text})}/>
+                    onChangeText={text => setPasswordTxt(text)}
+                    value={passwordTxt}/>
             </View>
             <TouchableOpacity>
                 <Text style={styles.forgot}>Forgot Password?</Text>
             </TouchableOpacity>
-            <TouchableOpacity style={styles.loginBtn}>
+            <TouchableOpacity style={styles.loginBtn} onPress={console.log(emailTxt)}>
                 <Text style={styles.loginText}>LOGIN</Text>
             </TouchableOpacity>
             <TouchableOpacity>
-                <Text style={styles.loginText}>Signup</Text>
+                <Text style={styles.loginText}>Sign Up</Text>
             </TouchableOpacity>
         </View>
     );
@@ -58,16 +60,16 @@ const styles = StyleSheet.create({
         color: 'white'
     },
     loginBtn:{
-        width:"80%",
-        backgroundColor:"#fb5b5a",
-        borderRadius:25,
-        height:50,
-        alignItems:"center",
-        justifyContent:"center",
-        marginTop:40,
-        marginBottom:10
+        width: '80%',
+        backgroundColor: '#fb5b5a',
+        borderRadius: 25,
+        height: 50,
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: 40,
+        marginBottom: 10
       },
     loginText:{
-        color:"white"
+        color: 'white'
     }
 });
