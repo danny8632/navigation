2d2971e5a02af05f39b7d44013373d00a78f9fb7
bensandland <benjaminsandland@gmail.com>
2020-12-07 11:07:29 +0100

    cleaned up a bit

--- App.js
+++ App.js
@@ -55,20 +55,16 @@ export default () => {
 	);
 
 	React.useEffect(() => {
-		// Fetch the token from storage then navigate to our appropriate place
+		// Fetch token using AsyncStorage. currently not used 
 		const bootstrapAsync = async () => {
 		  let storedToken;
 	
 		  try {
 			storedToken = await AsyncStorage.getItem('userToken');
 		  } catch (e) {
-			// Restoring token failed
+			// error handling
 		  }
 	
-		  // After restoring token, we may need to validate it in production apps
-	
-		  // This will switch to the App screen or Auth screen and this loading
-		  // screen will be unmounted and thrown away.
 		  dispatch({ type: 'RESTORE_TOKEN', token: storedToken });
 		};
 	
@@ -80,18 +76,10 @@ export default () => {
 	const authContext = React.useMemo(
 		() => ({
 		signIn: async data => {
-			// In a production app, we need to send some data (usually username, password) to server and get a token
-			// We will also need to handle errors if sign in failed
-			// After getting token, we need to persist the token using `AsyncStorage`
-			// In the example, we'll use a dummy token
 			dispatch({ type: 'SIGN_IN', token: 'dummy-auth-token' });
 		},
 		signOut: () => dispatch({ type: 'SIGN_OUT' }),
 		signUp: async data => {
-			// In a production app, we need to send user data to server and get a token
-			// We will also need to handle errors if sign up failed
-			// After getting token, we need to persist the token using `AsyncStorage`
-			// In the example, we'll use a dummy token
 
 			dispatch({ type: 'SIGN_IN', token: 'dummy-auth-token' });
 		},
